c65b6801491bbebd97aa1b35095025aec2b2e3c9
Jesus Unda <undajesusdavid@gmail.com>
2026-02-06 22:23:18 -0400

    ultimos cambios

--- src/shared/core/interfaces/unit-of-work.interface.ts
+++ src/shared/core/interfaces/unit-of-work.interface.ts
@@ -1,6 +1,5 @@
-export const UNIT_OF_WORK = Symbol('UNIT_OF_WORK');
+export const UNIT_OF_WORK = Symbol('UnitOfWork');
 
 export interface  IUnitOfWork {
-    
     runInTransaction<T>(work: () => Promise<T>): Promise<T>;
 }
--- src/shared/core/interfaces/uuid-service.interface.ts
+++ src/shared/core/interfaces/uuid-service.interface.ts
@@ -1,4 +1,3 @@
-
 export const UUID_SERVICE = Symbol("UuidService");
 
 export interface IUuidService {
