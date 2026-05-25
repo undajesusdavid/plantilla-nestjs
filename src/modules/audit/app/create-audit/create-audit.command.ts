e718c0839d96f1ec67210d36d510acf8aaa04c8f
Jesus Unda <undajesusdavid@gmail.com>
2026-05-25 10:59:37 -0400

    se elimino el modulo audit

--- src/modules/audit/app/create-audit/create-audit.command.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from '@shared/app/bus/command';
-
-export class CreateAuditCommand implements Command {
-  readonly name: string;
-
-  constructor(props: { name: string }) {
-    this.name = props.name;
-  }
-}
